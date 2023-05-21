7706ca89e58eb2b8873a5301a71f4a070cdfec17
Rohan <rsethi1@luc.edu>
2023-05-21 00:02:57 -0500

    added trajectory characteristics for two copies

--- script.bash
+++ script.bash
@@ -1,1 +1,3 @@
-nohup python3 processSimulations.py -generation -nstruct 1 -case /home/pkekeneshuskey/data/molecular_dynamics/ph_binsun/trajs3 &> ./timeseriesdata/inform3.out &
+nohup python3 processSimulations.py -generation -nstruct 2 -case /home/pkekeneshuskey/data/molecular_dynamics/ph_binsun/trajs3 &> ./timeseriesdatatwocopies/inform3.out8 &
+
+nohup python3 processSimulations.py -generation -nstruct 2 -case /home/pkekeneshuskey/data/molecular_dynamics/ph_binsun/trajs7_ad &> ./timeseriesdatatwocopies/inform7.out &
